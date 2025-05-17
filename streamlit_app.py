20fa6850dd571fd85b2c799f19b1037558dd216b
Siyabongangema <siyabonga.ngema@mukuru.com>
2025-05-18 00:58:36 +0200

    Update streamlit_app.py
    Changes made as final overall changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,50 @@
 # Import python packages
 import streamlit as st
+import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
+
 # Write directly to the app
-st.title("🥤Customize Your Smoothie!🥤")
+st.title("Customize your Smoothie!! :balloon:")
 st.write(
-  """Choose the fruits you want in your custom Smoothie!
-  """)
+"""
+This is test!!!
+""")
 
-name_on_order = st.text_input("Name on Smoothie:")
-st.write("The name on your Smoothie will be:", name_on_order)
-
-cnx = st.connection("snowflake")
+cnx=st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+
+name_on_order = st.text_input('Name on Smoothie: ')
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+
+pd_df=my_dataframe.to_pandas()
 
 
-ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:'
-    , my_dataframe
+ingredient_list = st.multiselect(
+    'Choose upto 5 ingradients: ', my_dataframe
     , max_selections=5
     )
 
+if ingredient_list:
+    ingredient_string = ''
 
-if ingredients_list:
-    ingredients_string = ''
+    for fruit_chosen in ingredient_list:
+        ingredient_string += fruit_chosen + ' '
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
+        st.subheader(fruit_chosen+ ' : Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
+        fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values ('""" + ingredient_string + """', '""" + name_on_order + """')"""
+ 
+    time_to_submit = st.button('Submit Order', type="primary")
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-
-    time_to_insert = st.button('Submit Order')
-    
-    if time_to_insert:
+    if time_to_submit:
         session.sql(my_insert_stmt).collect()
-
-        st.success('Your Smoothie is ordered!', icon="✅")   
+        st.success('Your Smoothie is ordered! ' +name_on_order, icon="✅")
 
